a5fba93a1fa475f310cc1313122880ade70f6781
juhis25 <123091089+juhis25@users.noreply.github.com>
2024-06-12 03:16:07 -0400

    Model_Training.R

--- Model_Training.R
+++ Model_Training.R
@@ -3,23 +3,39 @@ library(caret)
 library(randomForest)
 library(e1071)
 
-# Split the enhanced data into training and testing sets
+
+# Train a Random Forest model
 set.seed(123)
-train_index <- createDataPartition(glioma_df$grade, p = 0.8, list = FALSE)
-train_data <- glioma_df[train_index, ]
-test_data <- glioma_df[-train_index, ]
+rf_model <- randomForest(Grade ~ ., data = train_data, importance = TRUE)
+rf_predictions <- predict(rf_model, newdata = test_data, type = "response")
+rf_prob <- predict(rf_model, newdata = test_data, type = "prob")[,2]
 
-# Define the training control
-train_control <- trainControl(method = "cv", number = 10)
+# Confusion Matrix
+rf_cm <- confusionMatrix(rf_predictions, test_data$Grade)
+print(rf_cm)
 
-# Define the models to train
+# Train an SVM model
 set.seed(123)
-models <- list(
-    rf = train(grade ~ ., data = train_data, method = "rf", trControl = train_control),
-    svm = train(grade ~ ., data = train_data, method = "svmRadial", trControl = train_control),
-    glmnet = train(grade ~ ., data = train_data, method = "glmnet", trControl = train_control)
-)
+svm_model <- svm(Grade ~ ., data = train_data, probability = TRUE)
+svm_predictions <- predict(svm_model, newdata = test_data, probability = TRUE)
+svm_prob <- attr(predict(svm_model, newdata = test_data, probability = TRUE), 
+                 "probabilities")[,2]
 
-# Compare model performances
-results <- resamples(models)
-summary(results)
+# Confusion Matrix
+svm_cm <- confusionMatrix(svm_predictions, test_data$Grade)
+print(svm_cm)
+
+# Train a Neural Network model
+set.seed(123)
+nn_model <- nnet(Grade ~ ., data = train_data, size = 10, linout = FALSE)
+
+# Predict using the Neural Network model
+nn_predictions <- predict(nn_model, newdata = test_data, type = "class")
+nn_prob <- predict(nn_model, newdata = test_data, type = "raw")
+
+# Convert predictions to factors with the same levels as test_data$Grade
+nn_predictions <- factor(nn_predictions, levels = levels(test_data$Grade))
+
+# Confusion Matrix
+nn_cm <- confusionMatrix(nn_predictions, test_data$Grade)
+print(nn_cm)
